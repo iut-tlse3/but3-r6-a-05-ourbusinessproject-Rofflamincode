4bbf82227bb198137b00c94eabea55ab91deccb5
rmieg <romain.miegemolle@etu.iut-tlse3.fr>
2026-01-27 11:50:11 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,9 +1,17 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     @NotNull
     @Size(min=1)
     private String title;
